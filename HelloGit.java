a76987f7b999777661b7bcff06820026b406f5da
fb059 <39187273+fb059@users.noreply.github.com>
2020-09-21 21:30:32 +0900

    upload HelloGit

---
+++ HelloGit.java
@@ -0,0 +1,15 @@
+package kr.ac.jbnu.se.awp.gitplay5;
+
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git!!");
+		System.out.println("Hello hyeonjun!!");
+		System.out.println("Hello heejoo!!");
+		system.out.println("Hello im seongju!!!");
+		
+		for (int i=0;i<5;i++)
+			system.out.println(i);
+		system.out.println("This is a confliction test.");
+	}
+	//add commit : this seongju -> im modify
+}
